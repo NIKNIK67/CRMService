ea453df47fbdce7b38c4b2b598a2291ff6bd96ae
NIKNIK67 <1805niknik67@gmail.com>
2024-01-15 22:24:01 +0100

    fixes

--- ClientApp/src/app/document-management/document-management.component.ts
+++ ClientApp/src/app/document-management/document-management.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-document-management',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./document-management.component.css']
 })
 export class DocumentManagementComponent {
-
+  faFile = faFile
 }
